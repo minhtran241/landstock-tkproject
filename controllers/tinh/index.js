575d66ad384ed6de538c9ad6ebf40cc33dd13bbe
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-15 17:25:33 -0400

    fixed

--- controllers/tinh/index.js
+++ controllers/tinh/index.js
@@ -27,7 +27,7 @@ const getCityById = async (request, reply) => {
             format: 'JSONEachRow',
         });
         const city = await result.json();
-        console.log(typeof city);
+        console.log(city.iID_MaTinh);
         if (city === null) {
             // Handle the case where no data was found for the given ID
             reply.status(404).send({ error: 'City not found' });
