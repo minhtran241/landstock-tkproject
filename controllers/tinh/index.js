b73fee045dd17735eb24e833476ed1e024dbc545
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-15 22:46:26 -0400

    fixed

--- controllers/tinh/index.js
+++ controllers/tinh/index.js
@@ -62,11 +62,12 @@ const postCity = async (request, reply) => {
 
 const deleteCity = async (request, reply) => {
     const { id } = request.params;
-    const query = 'DELETE FROM tinh WHERE iID_MaTinh = {iID_MaTinh: Int64}';
+    const query = 'DELETE FROM tinh WHERE iID_MaTinh = {id: Int64}';
     try {
         await client.query({
             query,
-            query_params: { iID_MaTinh: Number(id) },
+            query_params: { id: Number(id) },
+            format: 'JSONEachRow',
         });
         reply.send({ message: 'city deleted successfully' });
     } catch (error) {
