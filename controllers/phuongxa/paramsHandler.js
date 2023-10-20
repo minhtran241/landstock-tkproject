a524c4e339fafb3c9c6c3bcd839e346363df561f
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-19 22:47:28 -0400

    cleaned the code

--- controllers/phuongxa/paramsHandler.js
+++ controllers/phuongxa/paramsHandler.js
@@ -4,7 +4,7 @@ const { table } = require('./constants');
 
 const getWardsQuery = (requestQuery) => {
     let query;
-    const { iID_MaQuan } = requestQuery;
+    let { iID_MaQuan } = requestQuery;
     if (iID_MaQuan) {
         iID_MaQuan = Number(iID_MaQuan);
         query = `SELECT iID_MaPhuongXa, sTenPhuongXa FROM ${table} WHERE iID_MaQuan = ${iID_MaQuan}`;
@@ -15,14 +15,14 @@ const getWardsQuery = (requestQuery) => {
 };
 
 const getWardByIdQuery = (requestParams) => {
-    const { id } = requestParams;
+    let { id } = requestParams;
     id = Number(id);
     const query = `SELECT iID_MaPhuongXa, sTenPhuongXa FROM ${table} WHERE iID_MaPhuongXa = ${id}`;
     return query;
 };
 
 const deleteWardByIdQuery = (requestParams) => {
-    const { id } = requestParams;
+    let { id } = requestParams;
     id = Number(id);
     const query = `ALTER TABLE ${table} DELETE WHERE iID_MaPhuongXa = ${id}`;
     return query;
