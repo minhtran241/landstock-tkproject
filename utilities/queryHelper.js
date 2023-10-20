3abc802901a8de2aefbaf46a8e0b715820f10e61
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-19 23:57:28 -0400

    updated code

--- utilities/queryHelper.js
+++ utilities/queryHelper.js
@@ -73,6 +73,9 @@ function cleanAndConvert(values) {
                 if (key.startsWith('i') || key.startsWith('f')) {
                     // Convert numeric attributes to numbers
                     cleanedValues[key] = Number(values[key]);
+                } else if (key.startsWith('s') && key.endsWith('s')) {
+                    // Convert string array attributes to string arrays. Example: sFiles
+                    cleanedValues[key] = values[key].split(',');
                 } else if (key.startsWith('s')) {
                     // Convert string attributes to strings
                     cleanedValues[key] = String(values[key]);
