a526b0c6b846759a453c4af1d8514bdca756b8a4
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-27 09:50:58 -0400

    added get all entries with func controller

--- utilities/funcParamsProcessing.js
+++ utilities/funcParamsProcessing.js
@@ -36,7 +36,7 @@ const queriesToResults = async (db_client, queries) => {
             format: 'JSONEachRow',
         });
         const data = await resultSet.json();
-        results[key] = data[0][`${key}()`];
+        results[key] = Number(data[0][`${key}()`]);
     }
     return results;
 };
