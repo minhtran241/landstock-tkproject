c8f06cfafda85e8e892f00baf18797842eef5699
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-26 23:20:00 -0400

    added get all entries with func controller

--- routes/bds/func/index.js
+++ routes/bds/func/index.js
@@ -1,5 +1,6 @@
 'use strict';
-const { getAllEntriesWithFunc } = require('../../controllers/bds');
+
+const { getAllEntriesWithFunc } = require('../../../controllers/bds');
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', opts, getAllEntriesWithFunc);
