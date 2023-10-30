d1d2b5c35df122cefc27df0af202512c6ecf6a0a
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-10-29 21:53:12 -0400

    debug

--- controllers/standard/index.js
+++ controllers/standard/index.js
@@ -59,7 +59,6 @@ const getEntryByIdStd = async (request, reply, po_Name, table) => {
             format: 'JSONEachRow',
         });
         let data = await rows.json();
-        console.log(data);
         // convertToType(po_Name, data);
         if (data !== null) {
             reply.code(200).send(data[0]);
