1fdd340acb6fd17a91f3c5f5bfd4fc5630b3572b
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-09 17:36:30 -0500

    updated jwt functions

--- plugins/jwt.js
+++ plugins/jwt.js
@@ -14,9 +14,9 @@ module.exports = fp(async function (fastify, opts) {
         verify: {
             // maxAge: null,
             algorithms: ['HS256'], // Specify the allowed verification algorithms
-            allowedIss: process.env.JWT_ISSUER, // Validate the issuer
-            allowedAud: process.env.JWT_AUDIENCE, // Validate the audience
-            allowedSub: process.env.JWT_PAYLOAD_SUBJECT, // Validate the subject
+            allowedIss: [process.env.JWT_ISSUER], // Validate the issuer
+            allowedAud: [process.env.JWT_AUDIENCE], // Validate the audience
+            allowedSub: [process.env.JWT_PAYLOAD_SUBJECT], // Validate the subject
         },
         cookie: {
             // Add cookies for storing JWT tokens
