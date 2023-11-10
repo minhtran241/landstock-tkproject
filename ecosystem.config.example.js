a3df9645433e03a4c245e24e61962a665a53c254
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-10 15:30:04 -0500

    fixed pm2 config file

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -18,5 +18,14 @@ module.exports = {
             max_size: '10M', // Optional: Rotate logs when the total size exceeds 10 megabytes
             keep_logs: 14, // Optional: Keep logs for the last 14 days
         },
+        {
+            name: 'landstock-api.main.dev',
+            script: 'npm',
+            args: 'start',
+            instances: 1, // Set the number of instances (nodes)
+            autorestart: true,
+            watch: true,
+            max_memory_restart: '1G',
+        },
     ],
 };
