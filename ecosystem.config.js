b090c20adc35538cbd81f7cff0eb4d5acb0dcec5
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-10 13:56:48 -0500

    added pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    apps: [
+        {
+            name: 'landstock-api.main.mbls',
+            script: 'npm',
+            args: 'start',
+            instances: 2, // Set the number of instances (nodes)
+            exec_mode: 'cluster', // Enable cluster mode
+            autorestart: true,
+            watch: true,
+            max_memory_restart: '1G',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            error_file: '/root/landstock-dev/logs/error.log',
+            out_file: '/root/landstock-dev/logs/out.log',
+            log_file: '/root/landstock-dev/logs/combined.log',
+            merge_logs: true,
+            log_rotate_date: true, // Enable log rotation based on date
+            max_size: '10M', // Optional: Rotate logs when the total size exceeds 10 megabytes
+            keep_logs: 14, // Optional: Keep logs for the last 14 days
+        },
+        {
+            name: 'landstock-api.main.dev',
+            script: 'npm',
+            args: 'start',
+            instances: 1, // Set the number of instances (nodes)
+            autorestart: true,
+            watch: true,
+            max_memory_restart: '1G',
+        },
+    ],
+};
--- plugins/winston.js
+++ plugins/winston.js
@@ -1,82 +1,85 @@
-'use strict';
+// 'use strict';
 
-const fp = require('fastify-plugin');
-const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+// const fp = require('fastify-plugin');
+// const winston = require('winston');
+// const DailyRotateFile = require('winston-daily-rotate-file');
 
-// the use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
+// // the use of fastify-plugin is required to be able
+// // to export the decorators to the outer scope
 
-module.exports = fp(async function (fastify, opts) {
-    // Set the log level based on the environment
-    const logLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug';
+// module.exports = fp(async function (fastify, opts) {
+//     // Set the log level based on the environment
+//     const logLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
-    // Configure Winston for custom logging
-    const logger = winston.createLogger({
-        level: logLevel,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json()
-        ),
-        transports: [
-            new winston.transports.Console(), // Log to console for both levels
-            new DailyRotateFile({
-                filename: '/root/landstock-dev/logs/info-%DATE%.log',
-                level: 'info', // Log info messages to info-<DATE>.log
-                datePattern: 'YYYY-MM-DD',
-                zippedArchive: true,
-                maxSize: '20m', // optional: rotate log files after 20 megabytes
-                maxFiles: '14d', // optional: keep logs for 14 days
-            }),
-            new DailyRotateFile({
-                filename: '/root/landstock-dev/logs/error-%DATE%.log',
-                level: 'error', // Log error messages to error-<DATE>.log
-                datePattern: 'YYYY-MM-DD',
-                zippedArchive: true,
-                maxSize: '20m',
-                maxFiles: '14d',
-            }),
-        ],
-    });
+//     // Configure Winston for custom logging
+//     const logger = winston.createLogger({
+//         level: logLevel,
+//         format: winston.format.combine(
+//             winston.format.timestamp(),
+//             winston.format.json()
+//         ),
+//         transports: [
+//             new winston.transports.Console(), // Log to console for both levels
+//             new DailyRotateFile({
+//                 filename: '/root/landstock-dev/logs/info-%DATE%.log',
+//                 level: 'info', // Log info messages to info-<DATE>.log
+//                 datePattern: 'YYYY-MM-DD',
+//                 zippedArchive: true,
+//                 maxSize: '20m', // optional: rotate log files after 20 megabytes
+//                 maxFiles: '14d', // optional: keep logs for 14 days
+//             }),
+//             new DailyRotateFile({
+//                 filename: '/root/landstock-dev/logs/error-%DATE%.log',
+//                 level: 'error', // Log error messages to error-<DATE>.log
+//                 datePattern: 'YYYY-MM-DD',
+//                 zippedArchive: true,
+//                 maxSize: '20m',
+//                 maxFiles: '14d',
+//             }),
+//         ],
+//     });
 
-    // Set the Fastify logger to the configured Winston logger
-    fastify.log = logger;
+//     // Set the Fastify logger to the configured Winston logger
+//     fastify.log = logger;
 
-    // Log every incoming request with additional details
-    fastify.addHook('onRequest', (request, reply, done) => {
-        const requestInfo = {
-            method: request.method,
-            url: request.url,
-            ip: request.ip,
-            userAgent: request.headers['user-agent'],
-            params: request.params,
-            query: request.query,
-            body: request.body,
-            headers: request.headers,
-        };
+//     // Log every incoming request with additional details
+//     fastify.addHook('onRequest', (request, reply, done) => {
+//         // Log successful requests
+//         if (request.statusCode < 400) {
+//             const requestInfo = {
+//                 method: request.method,
+//                 url: request.url,
+//                 ip: request.ip,
+//                 userAgent: request.headers['user-agent'],
+//                 params: request.params,
+//                 query: request.query,
+//                 body: request.body,
+//                 headers: request.headers,
+//             };
 
-        fastify.log.info({ request: requestInfo });
-        done();
-    });
+//             fastify.log.info({ request: requestInfo });
+//             done();
+//         }
+//     });
 
-    // Log errors
-    fastify.addHook('onError', (request, reply, error, done) => {
-        const errorInfo = {
-            method: request.method,
-            url: request.url,
-            ip: request.ip,
-            userAgent: request.headers['user-agent'],
-            params: request.params,
-            query: request.query,
-            body: request.body,
-            headers: request.headers,
-            error: {
-                message: error.message,
-                stack: error.stack,
-            },
-        };
+//     // Log errors
+//     fastify.addHook('onError', (request, reply, error, done) => {
+//         const errorInfo = {
+//             method: request.method,
+//             url: request.url,
+//             ip: request.ip,
+//             userAgent: request.headers['user-agent'],
+//             params: request.params,
+//             query: request.query,
+//             body: request.body,
+//             headers: request.headers,
+//             error: {
+//                 message: error.message,
+//                 stack: error.stack,
+//             },
+//         };
 
-        fastify.log.error({ error: errorInfo });
-        done();
-    });
-});
+//         fastify.log.error({ error: errorInfo });
+//         done();
+//     });
+// });
