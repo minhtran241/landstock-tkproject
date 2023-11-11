f1c2b3382870001396786d1d368bd795bf362f01
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-10 22:50:20 -0500

    provided healthcheck

---
+++ plugins/security/cors.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fp = require('fastify-plugin');
+
+module.exports = fp(async function (fastify, opts) {
+    fastify.register(require('@fastify/cors'), {
+        origin: [
+            `http://localhost:${process.env.FASTIFY_PORT}`,
+            `http://${process.env.FASTIFY_ADDRESS}:${process.env.FASTIFY_PORT}`,
+            'http://localhost:3000',
+        ],
+        methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    });
+});
