93cce14246f59f4589e73f0a7fac55703d872986
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-22 11:45:29 -0500

    moved private to .pem file

--- scripts/insert_csv_data.sh
+++ scripts/insert_csv_data.sh
@@ -28,7 +28,7 @@ echo "Inserting data into ClickHouse tables..."
 
 for i in "${!TABLE_NAMES[@]}"; do
     TABLE_NAME="${CLIENT_CODE}_${TABLE_NAMES[$i]}"
-    CSV_FILE="${TABLE_NAMES[$i]}.csv"
+    CSV_FILE="./data/csv/${TABLE_NAMES[$i]}.csv"
     
     echo "INSERT INTO ${CLICKHOUSE_DATABASE}.${TABLE_NAME} FORMAT CSVWithNames" < "${CSV_FILE}"
     clickhouse-client --user=${CLICKHOUSE_USER} --password=${CLICKHOUSE_PASSWORD} -q "INSERT INTO ${CLICKHOUSE_DATABASE}.${TABLE_NAME} FORMAT CSVWithNames" < "${CSV_FILE}"
