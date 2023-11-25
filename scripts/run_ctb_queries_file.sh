a4376f16dc450853b753c9fe83c948fb2723b9ca
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-11-24 22:45:30 -0500

    fixed scripts

--- scripts/run_ctb_queries_file.sh
+++ scripts/run_ctb_queries_file.sh
@@ -18,7 +18,7 @@ CLICKHOUSE_DATABASE=${CLICKHOUSE_DATABASE:-"landstock"}
 # SQL script file
 SQL_FILE="./queries/create-tables/${CLIENT_CODE}_tbs.sql"
 
-echo "USE ${CLICKHOUSE_DATABASE};" | cat - "$SQL_FILE" > temp && mv temp "$SQL_FILE" && rm temp
+echo "USE ${CLICKHOUSE_DATABASE};" | cat - "$SQL_FILE" > temp && mv temp "$SQL_FILE"
 SQL_FILE="./queries/create-tables/${CLIENT_CODE}_tbs.sql"
 
 # Execute the script
