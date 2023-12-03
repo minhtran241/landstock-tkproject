7192ad0b01f3d10c8804823b80974f51578b7985
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-12-02 21:06:59 -0500

    added custom callback

--- controllers/kh/index.js
+++ controllers/kh/index.js
@@ -2,6 +2,16 @@
 
 const axios = require('axios');
 
+axios.interceptors.request.use((request) => {
+    console.log('Starting Request', JSON.stringify(request, null, 2));
+    return request;
+});
+
+axios.interceptors.response.use((response) => {
+    console.log('Response:', JSON.stringify(response, null, 2));
+    return response;
+});
+
 const { po_KhachHang } = require('../../utilities/paramsOperations');
 const {
     getAllEntriesStd,
