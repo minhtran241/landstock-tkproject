bf8f88048af606f1e9725848574b79d4687fa78c
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-12-05 13:28:47 -0500

    worked on query params

--- data/clickhouse.js
+++ data/clickhouse.js
@@ -1,13 +1,22 @@
 'use strict';
-const clickhouse = require('@clickhouse/client');
+// const clickhouse = require('@clickhouse/client');
+const { ClickHouseClient } = require('@depyronick/clickhouse-client');
 
 // Create a Clickhouse client instance
-const client = clickhouse.createClient({
+// const client = clickhouse.createClient({
+//     host: process.env.CLICKHOUSE_HOST ?? 'http://localhost:8123',
+//     username: process.env.CLICKHOUSE_USER ?? 'default',
+//     password: process.env.CLICKHOUSE_PASSWORD ?? '',
+//     database: process.env.CLICKHOUSE_DATABASE ?? 'default',
+//     max_open_connections: process.env.CLICKHOUSE_MAX_OPEN_CONNECTIONS ?? 1,
+// });
+
+const client = new ClickHouseClient({
     host: process.env.CLICKHOUSE_HOST ?? 'http://localhost:8123',
     username: process.env.CLICKHOUSE_USER ?? 'default',
     password: process.env.CLICKHOUSE_PASSWORD ?? '',
     database: process.env.CLICKHOUSE_DATABASE ?? 'default',
-    max_open_connections: process.env.CLICKHOUSE_MAX_OPEN_CONNECTIONS ?? 1,
+    // max_open_connections: process.env.CLICKHOUSE_MAX_OPEN_CONNECTIONS ?? 1,
 });
 
 module.exports = client;
