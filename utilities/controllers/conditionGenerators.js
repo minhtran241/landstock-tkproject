669980b435f6e4ac8685e0e1ea64648e3b52544a
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-12-05 21:23:34 -0500

    worked on query params

--- utilities/controllers/conditionGenerators.js
+++ utilities/controllers/conditionGenerators.js
@@ -3,26 +3,26 @@
 /**
  * Converts a parameter and its value into a SQL condition.
  * @param {Object} po - The parameter operation object.
- * @param {Object} values - The values to use in the condition.
- * @returns {object} - An object containing the condition format and values for data binding.
+ * @param {Object} query_params - The query_params to use in the condition.
+ * @returns {object} - An object containing the condition format and query_params for data binding.
  */
-const paramToCondition = (po, values) => {
+const paramToCondition = (po, query_params) => {
     // Get the appropriate condition generator function or use the default
     const generateCondition =
         sqlConditionGenerators[po.o] || defaultConditionGenerator;
-    return generateCondition(po, values);
+    return generateCondition(po, query_params);
 };
 
 /**
  * Default condition generator for basic conditions.
  * @param {Object} po - The parameter operation object.
- * @param {Object} values - The values to use in the condition.
- * @returns {object} - An object containing the condition format and values for data binding.
+ * @param {Object} query_params - The query_params to use in the condition.
+ * @returns {object} - An object containing the condition format and query_params for data binding.
  */
-const defaultConditionGenerator = (po, values) => {
+const defaultConditionGenerator = (po, query_params) => {
     return {
         conditionFormat: `AND ${po.p} ${po.o} ${paramName}`,
-        values: { [po.p]: convertValueBasedOnType(po, values) },
+        query_params: { [po.p]: convertValueBasedOnType(po, query_params) },
     };
 };
 
@@ -30,54 +30,54 @@ const defaultConditionGenerator = (po, values) => {
  * Condition generators for special conditions.
  */
 const sqlConditionGenerators = {
-    IN: (po, values) => INCondition(po, values),
-    LIKEAND: (po, values) => LIKEANDCondition(po, values),
-    BETWEEN: (po, values) => BETWEENCondition(po, values),
+    IN: (po, query_params) => INCondition(po, query_params),
+    LIKEAND: (po, query_params) => LIKEANDCondition(po, query_params),
+    BETWEEN: (po, query_params) => BETWEENCondition(po, query_params),
 };
 
 /**
  * Condition generator for IN conditions.
  * @param {Object} pattr - The parameter attribute object.
- * @param {string} values - The values for the IN condition.
- * @returns {object} - An object containing the condition format and values for data binding.
+ * @param {string} query_params - The query_params for the IN condition.
+ * @returns {object} - An object containing the condition format and query_params for data binding.
  */
-const INCondition = (pattr, values) => {
+const INCondition = (pattr, query_params) => {
     // use data binding to prevent SQL injection
-    const vps = values
+    const vps = query_params
         .split(',')
         .map((val, index) => `{${pattr.p}:${pattr.clht}}`)
         .join(',');
-    const valueParams = values.split(',').reduce((params, val, index) => {
+    const valueParams = query_params.split(',').reduce((params, val, index) => {
         params[pattr.p] = val;
         return params;
     }, {});
     return {
         conditionFormat: `AND ${pattr.p} IN (${vps})`,
-        values: valueParams,
+        query_params: valueParams,
     };
 };
 
 /**
  * Condition generator for LIKE AND conditions.
  * @param {string} attr - The parameter attribute.
- * @param {string} values - The values for the LIKE AND condition.
- * @returns {object} - An object containing the condition format and values for data binding.
+ * @param {string} query_params - The query_params for the LIKE AND condition.
+ * @returns {object} - An object containing the condition format and query_params for data binding.
  */
-const LIKEANDCondition = (pattr, values) => {
+const LIKEANDCondition = (pattr, query_params) => {
     // use query parameters to prevent SQL injection
-    const likeConditions = values
+    const likeConditions = query_params
         .split(',')
         .map((val, index) => `${pattr.p} LIKE %{${pattr.p}:${pattr.clht}}%`)
         .join(' AND ');
 
-    const valueParams = values.split(',').reduce((params, val, index) => {
+    const valueParams = query_params.split(',').reduce((params, val, index) => {
         params[pattr.p] = val;
         return params;
     }, {});
 
     return {
         conditionFormat: `AND ${likeConditions}`,
-        values: valueParams,
+        query_params: valueParams,
     };
 };
 
@@ -85,7 +85,7 @@ const LIKEANDCondition = (pattr, values) => {
  * Condition generator for BETWEEN conditions.
  * @param {string} attr - The parameter attribute.
  * @param {string} rangeString - The range string for the BETWEEN condition.
- * @returns {object|null} - An object containing the condition format and values for data binding, or null if not applicable.
+ * @returns {object|null} - An object containing the condition format and query_params for data binding, or null if not applicable.
  */
 const BETWEENCondition = (pattr, rangeString) => {
     const rangeOperations = {
@@ -97,7 +97,7 @@ const BETWEENCondition = (pattr, rangeString) => {
                 const paramName = `${pattr.p}:${pattr.clht}`;
                 return {
                     conditionFormat: `AND ${pattr.p} = {${paramName}}`,
-                    values: { [pattr.p]: match[0] },
+                    query_params: { [pattr.p]: match[0] },
                 };
             },
         },
@@ -133,7 +133,7 @@ const BETWEENCondition = (pattr, rangeString) => {
                     conditionFormat: `AND ${minCond}${
                         minCond && maxCond ? ' AND ' : ''
                     }${maxCond}`,
-                    values: valueParams,
+                    query_params: valueParams,
                 };
             },
         },
